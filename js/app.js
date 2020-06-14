43630decff57fd9059edbdae9470f2ff1707013e
Waleed A. Afifi <waleedafifich@gmail.com>
2020-06-14 17:33:11 +0300

    Tokyo Object

--- js/app.js
+++ js/app.js
@@ -39,6 +39,46 @@ var Seattle = {
 
 Seattle.renderedArray();
 
+
+var Tokyo = {
+    location: 'Tokyo',
+    minCustomer: 3,
+    maxCustomer: 24,
+    avgCookies: 1.2,
+    opningTime: opningTime,
+    totalCookies: 0,
+    numberPerHour: generateRandomeCookies(this.minCustomer, this.maxCustomer),
+    renderedArray: function() {
+        var parentElement = document.getElementById('SalesReport');
+
+        var article = document.createElement('article');
+        parentElement.appendChild(article);
+
+        var h2 = document.createElement('h2');
+        h2.textContent = this.location;
+        article.appendChild(h2);
+
+        var ul = document.createElement('ul');
+        article.appendChild(ul);
+
+        for(var i = 0; i < this.opningTime.length; i++) {
+
+            var li = document.createElement('li');
+
+            this.numberPerHour = generateRandomeCookies(this.minCustomer, this.maxCustomer);
+            this.totalCookies += this.numberPerHour * Math.floor(this.avgCookies);
+            li.textContent = this.opningTime[i] + ' : ' + this.numberPerHour * Math.floor(this.avgCookies) + ' Cookies';
+            ul.appendChild(li);
+        }
+
+        var totlaLi = document.createElement('li');
+        totlaLi.textContent = 'Total : ' + this.totalCookies + ' Cookies';
+        ul.appendChild(totlaLi);
+    }
+};
+
+Tokyo.renderedArray()
+
 function generateRandomeCookies(min, max) {
     var randomValue = Math.random();
     return Math.floor(randomValue * (max - min + 1)) + min;
